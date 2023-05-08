2ebea93efbb9e9ff9ba328ba006616e14d2bc577
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-08 12:16:46 -0400

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 50 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
