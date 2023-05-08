721c5a4da112ef9ec14b992ef9fad584f592d3a5
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-08 13:09:41 -0400

    add Comment model, associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 50 }
