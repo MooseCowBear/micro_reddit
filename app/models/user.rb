6b73f780246271fdf581d7ad91274230558102d7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-08 12:45:38 -0400

    add Post model, add has_many association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, presence: true, length: { minimum: 2 }
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 50 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
